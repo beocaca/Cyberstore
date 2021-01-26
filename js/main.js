70764d5f90cbc5576be6c0886d0b2f88ea1e9fc8
beocaca <ceonamnguyen@gmail.com>
2021-01-26 07:25:12 +0700

    asd

--- js/main.js
+++ js/main.js
@@ -1,11 +1,4 @@
 window.onload = function () {
- /* $('#carousel-1').carousel({
-    interval: 4000,
-    wrap: true,
-    keyboard: true
-  });*/
-
-
   var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
   var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
     return new bootstrap.Tooltip(tooltipTriggerEl)
@@ -24,4 +17,28 @@ window.onload = function () {
     }*/
     // row--bor.innerHtml = htmlColum
   })
+
+
+  var carousel1 = document.getElementById("carouselExampleInterval")
+  carousel1.addEventListener('slide.bs.carousel', function (evt) {
+    // var active = document.querySelector('#carouselExampleInterval .carousel-item.active')
+    // active.classList.toggle('wow')
+    /*var {from, to} = evt
+    var nextItem = carousel1.querySelector('[data-reduce="' + to + '"]')
+    var prevItem = carousel1.querySelector('[data-reduce="' + from + '"]')
+    let clsWow = nextItem.getAttribute('data-wow')
+    let clsWowPre = prevItem.getAttribute('data-wow')
+    nextItem.classList.toggle('wow1')
+    nextItem.classList.toggle(clsWow)
+    prevItem.classList.remove('wow1')
+    prevItem.classList.remove(clsWowPre)
+    console.log(nextItem, clsWow)
+    new WOW().init({
+      boxClass: 'wow1',
+    });*/
+    // console.log(active)
+  })
+  // carousel1.addEventListener('slid.bs.carousel', (e) => {
+  //   console.log(arguments)
+  // })
 }
